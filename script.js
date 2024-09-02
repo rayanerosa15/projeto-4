87d24ad94ab369d8f8d7f779b4dffbf38d1108db
rayanerosa15 <134058761+rayanerosa15@users.noreply.github.com>
2024-09-02 20:46:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -82,3 +82,43 @@ const perguntas = [
         ]    
     }
 ];
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = ""; // Limpar alternativas antes de adicionar novas
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " "; // Concatenar a afirmação selecionada na história final
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "No final da tosa:"; // Texto final ajustado
+    textoResultado.textContent = historiaFinal.trim(); // Mostrar a história final completa
+    caixaAlternativas.textContent = ""; // Limpar alternativas
+}
+
+
+mostraPergunta()
